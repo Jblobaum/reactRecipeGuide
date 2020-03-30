03803c7e74047f4fdbcf92ecc8c914f3dc06f138
Jblobaum <justin.blobaum@midlandu.edu>
2020-03-30 01:19:53 -0500

    added get users and add user functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,44 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+
+admin.initializeApp()
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+ response.send("Hello world!");
+});
+
+exports.getUsers = functions.https.onRequest((request, response) => {
+    admin.firestore()
+    .collection('user')
+    .get()
+    .then(data => {
+        let users = [];
+        data.forEach(document => {
+            users.push(document.data())
+        })
+        return response.json(users);
+    })
+    .catch(err => console.log(err));
+});
+
+exports.createUser= functions.https.onRequest((request, response) => {
+    const newUser = {
+        username: request.body.username,
+        password: request.body.password
+    };
+    admin.firestore()
+    .collection('user')
+    .add(newUser)
+    .then((doc)=>{
+        response.json({ message: `User ${doc.id} created successfully.`})
+    })
+    .catch(err => {
+        response.status(500).json({ error: 'Something went wrong.'});
+        console.log(err);
+        
+    });
 });
