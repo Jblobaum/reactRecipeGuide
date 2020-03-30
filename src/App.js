f3bb3c86c498d1f61f258e1a870971ce4c8aade3
Jblobaum <justin.blobaum@midlandu.edu>
2020-03-30 00:09:15 -0500

    change direction

--- src/App.js
+++ src/App.js
@@ -13,27 +13,6 @@ import './App.css';
 
 
 const App = () => (
-  <MemoryRouter>
-
-    <Navbar className="justify-content-between" collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand>Recipe Guide</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav>
-        <LinkContainer to="/signup">
-        <Nav.Link>Sign Up</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/login">
-            <Nav.Link>Log In</Nav.Link>
-            </LinkContainer>
-        </Nav>
-
-        <Button pullRight variant="outline-info">Log Out</Button>
-      </Navbar.Collapse>
-    </Navbar>
-
-    <Routes />
-  </MemoryRouter>
 );
 
 
